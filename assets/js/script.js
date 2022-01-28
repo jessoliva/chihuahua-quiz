b450cd204d9ed934572a26f58dd178aa6f88cb35
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-01-27 22:39:20 -0600

    JS: referenced the screens

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// START SCREEN
+// reference Start Quiz <button> with id="start"
+const startBtn = document.getElementById('start');
+// reference start screen 
+const startScreen = document.getElementById('start-screen');
+
+// QUESTIONS SCREEN
+// reference questions screen
+const questionsScreen = document.getElementById('questions');
+// reference location where questions will display
+const questionText = document.getElementById('question-title');
+
+// END GAME SCREEN
+const endScreen = document.getElementById('end-screen');
+
+// LOSE SCREEN
+const loseScreen = document.getElementById('lose-screen');
